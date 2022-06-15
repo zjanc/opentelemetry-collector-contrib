e343c8e359854d1c8ace24c16bd1176e6b6f2307
Zjan Turla <zjan@canva.com>
2022-06-15 23:11:33 +0800

    write actual processor logic

--- processor.go
+++ processor.go
@@ -2,6 +2,7 @@ package filterspanprocessor
 
 import (
 	"context"
+	"time"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
@@ -28,5 +29,25 @@ func newFilterSpanProcessor(nextConsumer consumer.Traces, cfg *Config) (componen
 }
 
 func (fsp *filterspan) ProcessTraces(_ context.Context, batch ptrace.Traces) (ptrace.Traces, error) {
+	timeOffset := time.Nanosecond * time.Duration(fsp.StartTimeOffset)
+	lowerBound := time.Now().Add(timeOffset * -1)
+	upperBound := time.Now().Add(timeOffset)
+	batch.ResourceSpans().RemoveIf(func(rs ptrace.ResourceSpans) bool {
+		rs.ScopeSpans().RemoveIf(func(ss ptrace.ScopeSpans) bool {
+			ss.Spans().RemoveIf(func(s ptrace.Span) bool {
+				// This only accepts spans that are in the time range
+				// [time.Now() - offset, time.Now() + offset]
+				// Expression below checks if timestamp is out of bounds and drops if yes
+				oob := s.StartTimestamp().AsTime().Before(lowerBound) || s.StartTimestamp().AsTime().After(upperBound)
+				return oob
+			})
+			return ss.Spans().Len() == 0
+		})
+		return rs.ScopeSpans().Len() == 0
+	})
+	if batch.ResourceSpans().Len() == 0 {
+		return batch, processorhelper.ErrSkipProcessingData
+	}
+
 	return batch, nil
 }
--- processor_test.go
+++ processor_test.go
@@ -1,14 +1,19 @@
 package filterspanprocessor
 
 import (
+	"context"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
-func TestOne(t *testing.T) {
+func TestSpanFilter(t *testing.T) {
+	// Setup
 	ptrace.NewTraces()
 	traces := ptrace.NewTraces()
 	rs := traces.ResourceSpans().AppendEmpty()
@@ -23,12 +28,29 @@ func TestOne(t *testing.T) {
 	currentSpan.SetName("test-service")
 	currentSpan.SetStartTimestamp(pcommon.NewTimestampFromTime(currentTime))
 
-	futureTime := time.Now().Add(time.Hour)
+	futureTime := time.Now().Add(time.Hour * 2)
 	futureSpan.SetName("test-future-service")
 	futureSpan.SetStartTimestamp(pcommon.NewTimestampFromTime(futureTime))
 
-	pastTime := time.Now().Add(time.Hour * -1)
+	pastTime := time.Now().Add(time.Hour * -2)
 	pastSpan.SetName("test-past-service")
 	pastSpan.SetStartTimestamp(pcommon.NewTimestampFromTime(pastTime))
 
+	// Test
+	traceSink := new(consumertest.TracesSink)
+	cfg := &Config{
+		ProcessorSettings: config.NewProcessorSettings(config.NewComponentIDWithName(typeStr, typeStr)),
+		// 1 hour offset
+		StartTimeOffset: 3600000000000,
+	}
+	processor, _ := newFilterSpanProcessor(traceSink, cfg)
+	err := processor.ConsumeTraces(context.Background(), traces)
+
+	// Validate
+	assert.NoError(t, err)
+	assert.Len(t, traceSink.AllTraces(), 1)
+
+	// Assert that the correct spans got dropped
+	unfilteredSpans := traceSink.AllTraces()[0].ResourceSpans().At(0).ScopeSpans().At(0).Spans().At(0)
+	assert.Equal(t, "test-service", unfilteredSpans.Name())
 }
